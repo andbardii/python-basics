9f52dbb09f3353da82fad6c57ba51307c6d43148
andbardii <andbardii@icloud.com>
2023-09-25 22:16:52 +0200

    fix

--- g1/main.py
+++
@@ -1,4 +0,0 @@
-print("Here my first line of python!")
-
-# CREARE ENVIRONMENT SEPARATI PER I PROGETTI (commento in python!)
-print("python3 -m venv NOME_VIRTUAL_ENVIRONMENT")
---
+++ main.py
@@ -0,0 +1,7 @@
+# print("Here my first line of python!")
+
+#! CREARE ENVIRONMENT SEPARATI PER I PROGETTI 
+# print("python3 -m venv NOME_VIRTUAL_ENVIRONMENT")
+
+messaggio = input("inserisci il tuo nome:")
+print("Ciao " + messaggio + "!")
