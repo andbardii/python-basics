53e1bfecc4099dd8b1fca365ef8abfc7fca4036e
andbardii <andbardii@icloud.com>
2023-09-25 22:25:17 +0200

    add 003

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
+##### LEZIONE 001 ##### 
 # print("Here my first line of python!")
 
-#! CREARE ENVIRONMENT SEPARATI PER I PROGETTI 
+#* CREARE ENVIRONMENT SEPARATI PER I PROGETTI 
 # print("python3 -m venv NOME_VIRTUAL_ENVIRONMENT")
 
-messaggio = input("inserisci il tuo nome:")
-print("Ciao " + messaggio + "!")
+##### LEZIONE 002 ##### 
+# messaggio = input("inserisci il tuo nome:")
+# print("Ciao " + messaggio + "!")
+
+#* FARE ATTENZIONE ALL'INDENTAZIONE DEL CODICE
+#* NON SI USANO ;
+
+# if 1 < 5:
+#     print("minore")
+# else:
+#     print("maggiore")
+
+##### LEZIONE 003 ##### 
+#* NON SI PUO DICHIARARE UNA VARIABILE SENZA SPECIFICARE IL SUO VALORE
+
